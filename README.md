e2508e8e3dbe31c2123f00a060646e766e47d9e0
Jan Korte <j.korte@me.com>
2015-11-15 02:10:32 +0100

    no message

---
+++ README.md
@@ -0,0 +1,3 @@
+* Set URL and APIKEY in the script. The APIKEY is found in etherpad-lite/APIKEY.txt on the server.
+* Enter `pip install requests, click, etherpad_lite` in cmd.exe or terminal once
+* Enter `python export.py` in cmd.exe or terminal
---
+++ export.py
@@ -0,0 +1,20 @@
+from etherpad_lite import EtherpadLiteClient
+import os
+import requests
+import click
+
+URL = 'http://yourserver:yourport'
+APIKEY = 'inser your key here'
+
+client = EtherpadLiteClient(base_url=URL + '/api',
+                            base_params={'apikey': APIKEY},
+                            api_version='1.2.9')
+
+os.makedirs('exported_pads', exist_ok=True)
+
+pad_list = client.listAllPads()['padIDs']
+with click.progressbar(pad_list) as progress_bar:
+    for pad_name in progress_bar:
+        with open('exported_pads' + os.sep + pad_name + '.txt', 'w') as file:
+            pad_content = requests.get(URL + '/p/' + pad_name + '/export/txt').text
+            file.write(pad_content)
